f865fcef50054b95d667eac8d811bb698369586e
David Pi <david.pinho@gmail.com>
2021-02-14 17:26:43 +0000

    Changes to Costas Loop testing

--- tests/test_costas_loop.py
+++ tests/test_costas_loop.py
@@ -1,11 +1,13 @@
 import logging
 
+import matplotlib.pyplot as plt
 import numpy as np
 import sksdr
 
 _log = logging.getLogger(__name__)
 
 def test_costas_loop():
+
     loop_bw = 2 * np.pi / 100
     costas = sksdr.CostasLoop(loop_bw)
 
@@ -15,31 +17,30 @@ def test_costas_loop():
     # Generate random data symbols and apply BPSK modulation
     ints = np.random.randint(0, 2, 1000)
     bits = sksdr.x2binlist(ints, 1)
-    psk = sksdr.PSKModulator(sksdr.BPSK, [0, 1], 1.0, np.pi/4)
+    psk = sksdr.PSKModulator(sksdr.BPSK, [0, 1], 1.0)
     mod_sig = psk.modulate(bits)
 
     # Interpolate by 4 and filter with RRC tx filter
-    #interp = sksdr.FirInterpolator(4, sksdr.rrc(4, 0.5, 10))
-    #_, tx_sig = interp(mod_sig)
+    interp = sksdr.FirInterpolator(4, sksdr.rrc(4, 0.5, 10))
+    _, tx_sig = interp(mod_sig)
 
-    n = np.arange(0, 1000)
+    n = np.arange(0, 4000)
     Fs = 8e3
     F0 = 2e3
+    phase_offset = np.pi / 2
+    tx_carrier = np.exp(1j * (2 * np.pi * n * F0 / Fs + phase_offset))
+    rx_carrier = np.exp(-1j * 2 * np.pi * n * F0 / Fs)
 
-    #tx_sig = 1500 * np.exp(1j * (2 * np.pi * F0 / Fs * n + np.pi / 3))
-    tx_sig = np.repeat(1 * np.exp(1j * np.pi / 3), 1000)
+    rx_sig = tx_sig * tx_carrier * rx_carrier
 
     # Apply a delay offset
     #out_frame = vfd(tx_sig, 2)
-    out_frame = np.zeros_like(tx_sig)
-    expected_frame = np.empty_like(tx_sig)
-    costas(tx_sig, out_frame)
-
-    import matplotlib.pyplot as plt
+    out_sig = np.zeros_like(rx_sig)
+    costas(rx_sig, out_sig)
 
     plt.plot(tx_sig.real)
-    plt.plot(out_frame.real)
+    plt.plot(out_sig.real)
     plt.grid()
     plt.show()
 
-    assert np.allclose(out_frame, expected_frame)
+    #assert np.allclose(out_sig, expected_frame)
